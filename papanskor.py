7260f4d044e90ecf56ee2529a18e980e71b5102f
b3doy <the.b3doy@gmail.com>
2022-11-06 21:01:43 +0700

    edit filr papanskor.py dan menambahkan file baru yaitu data.txt untuk menyimpan highscore

--- papanskor.py
+++ papanskor.py
@@ -7,6 +7,8 @@ class Papanskor(Turtle):
     def __init__(self):
         super().__init__()
         self.skor = 0
+        with open("data.txt") as data_skor:
+            self.skor_tertinggi = int(data_skor.read()) 
         self.penup()
         self.color("white")
         self.setpos(0, 280)
@@ -14,13 +16,17 @@ class Papanskor(Turtle):
         self.update_papanskor()
 
     def update_papanskor(self):
-        self.write(f"Score: {self.skor}", move=False, align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.skor}, Highscore: {self.skor_tertinggi}", move=False, align=ALIGNMENT, font=FONT)
 
     def tambah_skor(self):
-        self.clear()
         self.skor += 1
         self.update_papanskor()
-    
+
     def game_over(self):
+        if self.skor > self.skor_tertinggi:
+            self.skor_tertinggi = self.skor
+            with open("data.txt", mode='w') as data_skor:
+                data_skor.write(str(self.skor_tertinggi))
         self.goto(0, 0)
         self.write(f"GAME OVER", move=False, align=ALIGNMENT, font=FONT)
